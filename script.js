3d7b55e4ced5732cca878630ab1635410e44441c
دانه المنيخ -197 <66743842+Danakwt@users.noreply.github.com>
2022-09-14 19:51:23 +0300

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+
+alert("welcome to my website! i am Dana, and i'll calculate your grades")
+
+let grade=prompt("Enter your grade")
+    console.log(grade)
+
+
+     if(grade >= 90 && grade >= 100){
+        console.log("لقد حصلت على امتياز🥳")
+     }else if(grade >= 80 && grade <= 89){
+        console.log("لقد حصلت على جيد جدا")
+     }else if(grade >= 70 && grade <= 79){
+        console.log("لقد حصلت على جيد ")
+    }else if(grade >= 60 && grade <= 69){
+        console.log("لقد حصلت على مقبول")
+    }else if(grade >= 50 && grade <= 59){
+        console.log("لقد حصلت على ضعيف")
+    }else if(grade > 50){
+        console.log("راسب")
+    }
+
+// java script isn't java
